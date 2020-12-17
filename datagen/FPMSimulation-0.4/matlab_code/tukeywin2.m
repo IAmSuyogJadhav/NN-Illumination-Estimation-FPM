7b5b7f575c28a990bf519ed810aa9e13aa629daf
IAmSuyogJadhav <suyog.jadhav1@gmail.com>
2020-12-18 02:26:35 +0530

    reorganized directory structure

--- datagen/FPMSimulation-0.4/matlab_code/tukeywin2.m
+++
@@ -1,8 +0,0 @@
-function g2 = tukeywin2(g, r)
-% applies a Tukey window with cosine fraction r.
-
-[M, N] = size(g);
-w1 = tukeywin(M,r)';
-w2 = tukeywin(N,r)';
-w = w1' * w2;
-g2 = g .* w;
--- fpm.py
+++ fpm.py
@@ -27,17 +27,38 @@ models = {
 }
 
 
-def get_illumination(tiff_path, model='multisize_raw', window='tukey', a=0.3, p=10, sig=230, do_psd=True, starting_angle=0, increase_angle=True, visualize=False, calibrate=True, fill_empty=True, tol='auto'):
+def load_model(model):
+    """
+    Parameters
+    -----------
+    `model`: String, Required
+        Which model to use. Defaults to multisize_raw. Currently available:
+        ['64_raw', '64_pre', '128_raw', '128_pre', '256_raw', '256_pre', '512_raw', '512_pre', 'multisize_raw']
+    """
+    assert model in models.keys(), "The model must be in the format {size}_{raw/pre}. For example:"\
+    " 128_pre for model trained on 128x128 preprocessed frames. Available models:\n"\
+    f"{models.keys()}"
+
+    # Load the model
+    print('Loading the model...', end='')
+    cfg_path, preprocess_fft = models[model]
+    cfg = pickle.load(open(os.path.join(cfg_path, 'cfg.pkl'), 'rb'))
+    cfg.MODEL.WEIGHTS = os.path.join(cfg_path, 'model_final.pth')
+    predictor = Predictor(cfg, preprocess_fft=preprocess_fft)
+    print('Done!')
+    return predictor
+
+
+def get_illumination(tiff_path, predictor, window='tukey', a=0.3, p=10, sig=230, do_psd=True, starting_angle=0, increase_angle=True, visualize=False, calibrate=True, fill_empty=True, tol='auto'):
     """
     get_illumination(model)
-    
+
     Parameters
     ----------
     `tiff_path`: String, Required.
-            Path to the tiff file.
-    `model`: String, Optional.
-        Which model to use. Defaults to multisize_raw. Currently available: 
-        ['64_raw', '64_pre', '128_raw', '128_pre', '256_raw', '256_pre', '512_raw', '512_pre', 'multisize_raw']
+        Path to the tiff file.
+    `predictor`: Detectron2.DefaultPredictor instance or String, Required.
+        The loaded model or its name (one of ['64_raw', '64_pre', '128_raw', '128_pre', '256_raw', '256_pre', '512_raw', '512_pre', 'multisize_raw']).
     `window`: String, Optional
         Which window to use for apodization before estimating illumination. Defaults to 'tukey'.
         Available options: ['gaussian', 'tukey', None]
@@ -55,50 +76,41 @@ def get_illumination(tiff_path, model='multisize_raw', window='tukey', a=0.3, p=
     `visualize`: Boolean, Optional
         Defaults to False. Whether to visualize te disc locations using a matplotlib plot.
     `calibrate`: Boolean, Optional
-        Defaults to True. Whether to use calibration or not. 
+        Defaults to True. Whether to use calibration or not.
     `fill_empty`: Boolean, Optional
-        Defaults to True. Whether to fill frames that have no detection with estimated disc locations. 
+        Defaults to True. Whether to fill frames that have no detection with estimated disc locations.
     `tol`: String, Optional
         Defaults to 'auto'. Tolerance value for rejecting wrong detections. If set to None or 'none', does not reject any detection.
-    
+
     """
-    
-    assert model in models.keys(), "The model must be in the format {size}_{raw/pre}. For example:"\
-    " 128_pre for model trained on 128x128 preprocessed frames. Available models:\n"\
-    f"{models.keys()}"
-    
-    # Load the model
-    print('Loading the model...', end='')
-    cfg_path, preprocess_fft = models[model]
-    cfg = pickle.load(open(os.path.join(cfg_path, 'cfg.pkl'), 'rb'))
-    cfg.MODEL.WEIGHTS = os.path.join(cfg_path, 'model_final.pth')
-    predictor = Predictor(cfg, preprocess_fft=preprocess_fft)
-    print('Done!')
-    
+
+    if isinstance(predictor, str):
+        predictor = load_model(predictor)
+
     # Read the tiff file
     imgs = read_tiff(tiff_path)
-    
+
     if imgs[0].shape[0] != imgs[0].shape[1]:
         print('The input image is not square. The image will be cropped to be a*a where `a` is the smallest dimension.')
         a = min(imgs[0].shape[0], imgs[0].shape[1])
         slice_x, slice_y = slice(0, a), slice(0, a)
         imgs = [img[slice_x, slice_y] for img in imgs]
-    
+
     # Apodization
     width, height = imgs[0].shape  # Images aren't supposed to have 3rd dimension
     if window is not None and window.lower()=='gaussian':
         w = np.outer(signal.general_gaussian(width, p=p, sig=sig), signal.general_gaussian(width, p=p, sig=sig))
     elif window is not None and window.lower()=='tukey':
         w = np.outer(signal.tukey(width, alpha=a), signal.tukey(height, alpha=a))
-    elif window is None or window.lower() is 'none':
+    else:
         w=1
 
     imgs = [w*img for img in tqdm(imgs, desc='Processing Apodization', leave=False)]
-    
+
     # Periodic Smooth Decomposition
     if do_psd:
         imgs = [psd(img)[0] for img in tqdm(imgs, desc='Processing PSD', leave=False)]
-    
+
     # Figure out idxs
     if (starting_angle == 0) and increase_angle:  # The default case
         idxs = None
@@ -111,7 +123,7 @@ def get_illumination(tiff_path, model='multisize_raw', window='tukey', a=0.3, p=
         else:
             thetas_ = thetas[offset::-1] + thetas[- (len(imgs) - offset -1):][::-1]
             idxs = [thetas.index(t) for t in thetas_]
-    
+
     # Estimate illumination
     try:
         discs, radii = predictor.get_disc(
@@ -123,16 +135,16 @@ def get_illumination(tiff_path, model='multisize_raw', window='tukey', a=0.3, p=
             idxs=idxs,
             tol=tol
         )
-        
+
         return discs, radii
-    
+
     except IndexError:
         print(
             '\n [!] The model did not return any detections.'\
             ' Check to make sure the arguments starting_angle and increase_angle are set correctly.'\
             'You can also try changing the model, changing windowing method, turn PSD on/off, set tol to None, etc. to see if it helps.'
          )
-        
+
         exit(0)
 
 
@@ -145,35 +157,35 @@ def get_reconstruction(tiff_path, discs, row, params):
         a = min(imgs[0].shape[0], imgs[0].shape[1])
         slice_x, slice_y = slice(0, a), slice(0, a)
         imgs = [img[slice_x, slice_y] for img in imgs]
-    
+
     window = params['window']
     a, p, sig = params['a'], params['p'], params['sig']
     do_psd = params['do_psd']
-    
+
     # Apodization
     width, height = imgs[0].shape  # Images aren't supposed to have 3rd dimension
     if window is not None and window.lower()=='gaussian':
         w = np.outer(signal.general_gaussian(width, p=p, sig=sig), signal.general_gaussian(width, p=p, sig=sig))
     elif window is not None and window.lower()=='tukey':
         w = np.outer(signal.tukey(width, alpha=a), signal.tukey(height, alpha=a))
-    elif window is None or window.lower() is 'none':
+    else:
         w=1
 
     imgs = [w*img for img in tqdm(imgs, desc='Processing Apodization', leave=False)]
-    
+
     # Periodic Smooth Decomposition
     if do_psd:
         imgs = [psd(img)[0] for img in tqdm(imgs, desc='Processing PSD', leave=False)]
-    
+
     imgs = [cp.array(img) for img in imgs]  # Transfer to GPU
 
     IMAGESIZE = imgs[0].shape[0]
-    scale = params['scale']            
+    scale = params['scale']
     hres_size = (IMAGESIZE * scale, IMAGESIZE * scale)
 
     # Remove keys not used by the reconstruction algo
     prms = {k: params[k] for k in params.keys() - ['scale', 'do_psd', 'window', 'a', 'p', 'sig']}
-    
+
     # Reconstruction
     print('Performing Reconstruction...', end='')
     obj, pupil = reconstruct_v2(
@@ -184,10 +196,10 @@ def get_reconstruction(tiff_path, discs, row, params):
         **prms
     )
     print('Done!')
-    
+
     return obj, pupil, imgs
 
-        
+
 def save_illumination(discs, radii, tiff_path, params):
     print('Saving illumination results...', end='')
     os.makedirs(params['illumination']['savedir'], exist_ok=True)
@@ -225,6 +237,8 @@ def save_illumination(discs, radii, tiff_path, params):
     else:
         print(f"{params['illumination']['format']} format not recognised. Only mat and npz are supported.")
 
+    return savepath
+
 
 def save_reconstruction(obj, pupil, imgs, tiff_path, params):
     print('Saving reconstruction results...', end='')
@@ -238,73 +252,86 @@ def save_reconstruction(obj, pupil, imgs, tiff_path, params):
     if params['reconstruction']['format'].lower() == 'png':
         # Amp
         im = cp.asnumpy(to_uint8(cp.abs(obj)))
-        cv2.imwrite(unique_path(savepath + '_amp.png'), im)
-        
+
+        savepath1 = unique_path(savepath + '_amp.png')
+        cv2.imwrite(savepath1, im)
+
         # Phase
         im = cp.asnumpy(to_uint8(cp.angle(obj)))
-        cv2.imwrite(unique_path(savepath + '_phase.png'), im)
-        
+        savepath2 = unique_path(savepath + '_phase.png')
+        cv2.imwrite(savepath2, im)
+
         # Mean Image
         mean_img = cp.array(imgs).mean(axis=0)
         im = cp.asnumpy(to_uint8(mean_img))
-        cv2.imwrite(unique_path(savepath + '_raw_mean.png'), im)
+        savepath3 = unique_path(savepath + '_raw_mean.png')
+        cv2.imwrite(savepath3, im)
 
         # Pupil Amp
         im = cp.asnumpy(to_uint8(cp.abs(pupil)))
-        cv2.imwrite(unique_path(savepath + '_pupil_amp.png'), im)
-        
+        savepath4 = unique_path(savepath + '_pupil_amp.png')
+        cv2.imwrite(savepath4, im)
+
         # Pupil Phase
         im = cp.asnumpy(to_uint8(cp.angle(pupil)))
-        cv2.imwrite(unique_path(savepath + '_pupil_phase.png'), im)
+        savepath5 = unique_path(savepath + '_pupil_phase.png')
+        cv2.imwrite(savepath5, im)
         print('Done!')
-        
+
     elif params['reconstruction']['format'].lower() == 'tiff':
         # Amp
+        savepath1 = unique_path(savepath + '_amp.tiff')
         skimage.io.imsave(
-            unique_path(savepath + '_amp.tiff'),
+            savepath1,
             cp.asnumpy(cp.abs(obj))
         )
-        
+
         # Phase
+        savepath2 = unique_path(savepath + '_phase.tiff')
         skimage.io.imsave(
-            unique_path(savepath + '_phase.tiff'),
+            savepath2,
             cp.asnumpy(cp.angle(obj))
         )
-        
+
         # Mean Image
         mean_img = cp.array(imgs).mean(axis=0)
+        savepath3 = unique_path(savepath + '_raw_mean.tiff')
         skimage.io.imsave(
-            unique_path(savepath + '_raw_mean.tiff'),
+            savepath3,
             cp.asnumpy(mean_img)
         )
-        
+
         # Pupil Amp
+        savepath4 = unique_path(savepath + '_pupil_amp.tiff')
         skimage.io.imsave(
-            unique_path(savepath + '_pupil_amp.tiff'),
+            savepath4,
             cp.asnumpy(cp.abs(pupil))
         )
-        
+
         # Pupil Phase
+        savepath5 = unique_path(savepath + '_pupil_phase.tiff')
         skimage.io.imsave(
-            unique_path(savepath + '_pupil_phase.tiff'),
+            savepath5,
             cp.asnumpy(cp.angle(pupil))
         )
         print('Done!')
     else:
         print(f"{params['reconstruction']['format']} format not recognised. Only png and tiff are supported.")
 
-        
+    return savepath1, savepath2, savepath3, savepath4, savepath5
+
+
 def unique_path(f):
     """
     Creates unique path.
     """
     i = 1
     name, ext = os.path.splitext(f)
-    
+
     while os.path.exists(f):
         f = f'{name}_{i}{ext}'
         i += 1
-    
+
     return f
 
 
@@ -320,7 +347,7 @@ if __name__ == '__main__':
 
     tiff_path = argv[1]
     params = argv[2].rstrip('.py')
-    
+
     exec(f'from {params} import illumination_params, save_params, row, reconstruction_params')
 
     # Illumination Estimation
@@ -331,6 +358,6 @@ if __name__ == '__main__':
 
     # Reconstruction
     obj, pupil, imgs = get_reconstruction(tiff_path, discs, row, reconstruction_params)
-    
+
     # Save Reconstruction Results
     save_reconstruction(obj, pupil, imgs, tiff_path, save_params)
